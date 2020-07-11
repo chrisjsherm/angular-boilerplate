f3fbaf77fe3b1c0ec0e9ee7c964b5f7794e15284
Chris Sherman <chris@shermandigital.com>
2020-07-11 11:44:55 -0400

    Refactor Husky (Git hooks) configuration file

--- .huskyrc.js
+++ .huskyrc.js
@@ -1,6 +1,11 @@
+const generateTasksCommand = (arr) => arr.join(' && ');
+
 module.exports = {
   hooks: {
-    'pre-commit':
-      'pretty-quick --staged && npm run lint && npm run test -- --watch=false',
+    'pre-commit': generateTasksCommand([
+      'pretty-quick --staged',
+      'npm run lint',
+      'npm run test -- --watch=false',
+    ]),
   },
 };
