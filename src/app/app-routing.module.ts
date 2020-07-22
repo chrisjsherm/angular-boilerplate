8822399e8f8879fd4c9bbe4877cabf78191452c7
Chris Sherman <chris@shermandigital.com>
2020-07-22 17:37:44 -0400

    Hero detail page routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ListPageComponent } from './pages/list-page/list-page.component';
+import { DetailPageComponent } from './pages/detail-page/detail-page.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/heroes', pathMatch: 'full' },
   { path: 'heroes', component: ListPageComponent },
+  { path: 'heroes/:uuid', component: DetailPageComponent },
 ];
 
 /**
