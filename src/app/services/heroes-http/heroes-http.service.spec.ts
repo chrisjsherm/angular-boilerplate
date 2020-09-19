d905ac263eec60b07da529cd357e69aa597a32a8
Chris Sherman <chris@shermandigital.com>
2020-09-19 10:34:32 -0400

    Update/PATCH heroes HTTP endpoint

--- src/app/services/heroes-http/heroes-http.service.spec.ts
+++ src/app/services/heroes-http/heroes-http.service.spec.ts
@@ -1,12 +1,11 @@
-import { TestBed } from '@angular/core/testing';
-
-import { HeroesHttpService } from './heroes-http.service';
 import {
   HttpClientTestingModule,
   HttpTestingController,
 } from '@angular/common/http/testing';
+import { TestBed } from '@angular/core/testing';
 import { RuntimeConfigurationService } from '../runtime-configuration/runtime-configuration.service';
 import { MockRuntimeConfigurationService } from '../runtime-configuration/testing/mock-runtime-configuration.service';
+import { HeroesHttpService } from './heroes-http.service';
 
 describe('HeroesHttpService', (): void => {
   let service: HeroesHttpService;
@@ -45,5 +44,29 @@ describe('HeroesHttpService', (): void => {
       'http://192.1.1.1:8282/api/v1/heroes',
     );
     expect(testRequest.request.method).toBe('GET');
+  });
+
+  it('should update the hero', (): void => {
+    // Act
+    service
+      .update('db3ee04b-05be-4403-9d48-807fb29717ec', {
+        firstName: 'G.',
+        lastName: 'Washington',
+        phoneNumber: '(703) 111-1111',
+        avatarUrl: 'https://avatar.com/george-washington/profile.jpg',
+      })
+      .subscribe();
+
+    // Assert
+    const testRequest = httpTestingController.expectOne(
+      'http://192.1.1.1:8282/api/v1/heroes/db3ee04b-05be-4403-9d48-807fb29717ec',
+    );
+    expect(testRequest.request.method).toBe('PATCH');
+    expect(testRequest.request.body).toEqual({
+      firstName: 'G.',
+      lastName: 'Washington',
+      phoneNumber: '(703) 111-1111',
+      avatarUrl: 'https://avatar.com/george-washington/profile.jpg',
+    });
   });
 });
--- src/app/services/heroes-http/heroes-http.service.ts
+++ src/app/services/heroes-http/heroes-http.service.ts
@@ -1,9 +1,10 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { RuntimeConfigurationService } from '../runtime-configuration/runtime-configuration.service';
-import { HttpClient } from '@angular/common/http';
-import { map, switchMap } from 'rxjs/operators';
+import { exhaustMap, map, switchMap } from 'rxjs/operators';
 import { Hero } from '../../models/hero.entity';
+import { HeroFormValues } from '../../pages/edit-page/hero-form-values.interface';
+import { RuntimeConfigurationService } from '../runtime-configuration/runtime-configuration.service';
 
 /**
  * Interact with the Heroes HTTP API
@@ -33,6 +34,24 @@ export class HeroesHttpService {
       switchMap(
         (baseUrl: string): Observable<Hero[]> =>
           this.httpClient.get<Hero[]>(baseUrl),
+      ),
+    );
+  }
+
+  /**
+   * Update fields accessible via the edit form
+   *
+   * @param id ID of the hero we're updating
+   * @param formValues Updatable fields
+   *
+   * @returns Empty Observable indicating the completion of the HTTP request
+   */
+  update(id: string, formValues: HeroFormValues): Observable<void> {
+    return this.baseUrl$.pipe(
+      exhaustMap(
+        (baseUrl: string): Observable<void> => {
+          return this.httpClient.patch<void>(`${baseUrl}/${id}`, formValues);
+        },
       ),
     );
   }
