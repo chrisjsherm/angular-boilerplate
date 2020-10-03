06dd9d4fcb45756b5c33bcbb3718f56d9cacf965
Chris Sherman <chris@shermandigital.com>
2020-10-03 12:02:00 -0500

    Issue success or failure snackbar on update

--- src/app/pages/edit-page/redux/effects/submit-edit-form.effects.ts
+++ src/app/pages/edit-page/redux/effects/submit-edit-form.effects.ts
@@ -1,9 +1,13 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { Action } from '@ngrx/store';
 import { Observable, of } from 'rxjs';
-import { catchError, exhaustMap, map } from 'rxjs/operators';
+import { catchError, exhaustMap, map, tap } from 'rxjs/operators';
+import { SnackbarFailureComponent } from '../../../../atoms/snackbar-failure/snackbar-failure.component';
+import { SnackbarSuccessComponent } from '../../../../atoms/snackbar-success/snackbar-success.component';
 import { HeroesHttpService } from '../../../../services/heroes-http/heroes-http.service';
 import {
   submitEditForm,
@@ -19,6 +23,8 @@ export class SubmitEditFormEffects {
   constructor(
     private actions$: Actions,
     private heroesHttpService: HeroesHttpService,
+    private router: Router,
+    private snackbarService: MatSnackBar,
   ) {}
 
   submitEditForm$ = createEffect(
@@ -38,8 +44,17 @@ export class SubmitEditFormEffects {
                     });
                   },
                 ),
+                tap((): void => {
+                  this.snackbarService.openFromComponent(
+                    SnackbarSuccessComponent,
+                  );
+                  this.router.navigate(['/heroes']);
+                }),
                 catchError(
                   (error: HttpErrorResponse): Observable<Action> => {
+                    this.snackbarService.openFromComponent(
+                      SnackbarFailureComponent,
+                    );
                     return of(
                       submitEditFormFailure({
                         id: action.id,
