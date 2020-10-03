84c75a879ff438a306c4a12fbea04300f1f5c47a
Chris Sherman <chris@shermandigital.com>
2020-10-03 11:57:24 -0500

    Configure global Angular Material error state matcher

--- src/app/angular-material/angular-material-re-export.module.ts
+++ src/app/angular-material/angular-material-re-export.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import {
+  ErrorStateMatcher,
+  ShowOnDirtyErrorStateMatcher,
+} from '@angular/material/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatIconModule } from '@angular/material/icon';
@@ -8,6 +12,10 @@ import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import {
+  MatSnackBarModule,
+  MAT_SNACK_BAR_DEFAULT_OPTIONS,
+} from '@angular/material/snack-bar';
 import { MatSortModule } from '@angular/material/sort';
 import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -25,10 +33,15 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     MatProgressSpinnerModule,
     MatSortModule,
     MatSidenavModule,
+    MatSnackBarModule,
     MatToolbarModule,
     MatIconModule,
     MatListModule,
     MatGridListModule,
+  ],
+  providers: [
+    { provide: ErrorStateMatcher, useClass: ShowOnDirtyErrorStateMatcher },
+    { provide: MAT_SNACK_BAR_DEFAULT_OPTIONS, useValue: { duration: 10500 } },
   ],
 })
 export class AngularMaterialReExportModule {}
