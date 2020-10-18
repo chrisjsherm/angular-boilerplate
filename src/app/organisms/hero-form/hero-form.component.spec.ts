a0c7ea7889aacb81816ac195ba7306f7d1d27b93
Chris Sherman <chris@shermandigital.com>
2020-10-18 19:08:38 -0400

    Implement hero form via ControlValueAccessor and Validator interfaces

---
+++ src/app/organisms/hero-form/hero-form.component.spec.ts
@@ -0,0 +1,156 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { MatError, MatFormField, MatLabel } from '@angular/material/form-field';
+import { MockComponents } from 'ng-mocks';
+import { HeroFormValues } from '../../models/hero-form-values.interface';
+import { HeroFormComponent } from './hero-form.component';
+
+describe('HeroFormComponent', (): void => {
+  let component: HeroFormComponent;
+  let fixture: ComponentFixture<HeroFormComponent>;
+
+  beforeEach(
+    async (): Promise<void> => {
+      await TestBed.configureTestingModule({
+        imports: [ReactiveFormsModule],
+        declarations: [
+          HeroFormComponent,
+          MockComponents(MatLabel, MatError, MatFormField),
+        ],
+        providers: [FormBuilder],
+      }).compileComponents();
+    },
+  );
+
+  beforeEach((): void => {
+    fixture = TestBed.createComponent(HeroFormComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', (): void => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should call onChange and onTouched when a form value changes', (): void => {
+    // Assert
+    expect(component.formGroup.value).toEqual({
+      firstName: '',
+      lastName: '',
+      phoneNumber: '',
+      avatarUrl: '',
+    });
+
+    // Arrange
+    const onChangeSpy = spyOn(component, 'onChange');
+    const onTouchedSpy = spyOn(component, 'onTouched');
+
+    // Act
+    component.formGroup.get('firstName').setValue('Bruce');
+
+    // Assert
+    expect(onChangeSpy).toHaveBeenCalledTimes(1);
+    expect(onChangeSpy).toHaveBeenCalledWith({
+      firstName: 'Bruce',
+      lastName: '',
+      phoneNumber: '',
+      avatarUrl: '',
+    });
+    expect(onTouchedSpy).toHaveBeenCalledTimes(1);
+  });
+
+  describe('Set disabled state', (): void => {
+    it('should disable the form group', (): void => {
+      // Act
+      component.setDisabledState(true);
+
+      // Assert
+      expect(component.formGroup.disabled).toBe(true);
+    });
+
+    it('should enable the form group', (): void => {
+      // Act
+      component.setDisabledState(false);
+
+      // Assert
+      expect(component.formGroup.enabled).toBe(true);
+    });
+  });
+
+  describe('Validation', (): void => {
+    it('should determine the form is invalid', (): void => {
+      // Assert
+      expect(component.formGroup.value).toEqual({
+        firstName: '',
+        lastName: '',
+        phoneNumber: '',
+        avatarUrl: '',
+      });
+
+      // Act
+      const result = component.validate();
+
+      // Assert
+      expect(result).toEqual({
+        firstName: { required: true },
+        lastName: { required: true },
+        phoneNumber: { required: true },
+        avatarUrl: { required: true },
+      });
+    });
+
+    it('should determine the form is valid', (): void => {
+      // Arrange
+      component.formGroup.setValue({
+        avatarUrl: 'https://avatar.com/george-washington/profile.jpg',
+        firstName: 'George',
+        lastName: 'Washington',
+        phoneNumber: '(703) 111-1111',
+      });
+
+      // Act
+      const result = component.validate();
+
+      // Assert
+      expect(result).toBeNull();
+    });
+  });
+
+  describe('Write to the form control from outside the form contorl component', (): void => {
+    it('should not update the from group if the value is falsy', (): void => {
+      // Arrange
+      const setValueSpy = spyOn(component.formGroup, 'setValue');
+
+      // Act
+      component.writeValue(('' as unknown) as HeroFormValues);
+
+      // Assert
+      expect(setValueSpy).not.toHaveBeenCalled();
+    });
+
+    it('should update the from group', (): void => {
+      // Arrange
+      const setValueSpy = spyOn(component.formGroup, 'setValue');
+
+      // Act
+      component.writeValue({
+        avatarUrl: 'https://avatar.com/george-washington/profile.jpg',
+        firstName: 'George',
+        lastName: 'Washington',
+        phoneNumber: '(703) 111-1111',
+      });
+
+      // Assert
+      expect(setValueSpy).toHaveBeenCalledTimes(1);
+      expect(setValueSpy).toHaveBeenCalledWith(
+        {
+          avatarUrl: 'https://avatar.com/george-washington/profile.jpg',
+          firstName: 'George',
+          lastName: 'Washington',
+          phoneNumber: '(703) 111-1111',
+        },
+        { emitEvent: false },
+      );
+    });
+  });
+});
---
+++ src/app/organisms/hero-form/hero-form.component.ts
@@ -0,0 +1,148 @@
+import { Component, OnDestroy } from '@angular/core';
+import {
+  ControlValueAccessor,
+  FormBuilder,
+  FormGroup,
+  NG_VALIDATORS,
+  NG_VALUE_ACCESSOR,
+  ValidationErrors,
+  Validator,
+  Validators,
+} from '@angular/forms';
+import { Subscription } from 'rxjs';
+import { HeroFormValues } from '../../models/hero-form-values.interface';
+
+/**
+ * Custom form control for Hero properties. Enables us to use the same component
+ * for both create and edit pages.
+ */
+@Component({
+  selector: 'app-hero-form',
+  templateUrl: './hero-form.component.html',
+  styleUrls: ['./hero-form.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: HeroFormComponent,
+      multi: true,
+    },
+    {
+      provide: NG_VALIDATORS,
+      useExisting: HeroFormComponent,
+      multi: true,
+    },
+  ],
+})
+export class HeroFormComponent
+  implements ControlValueAccessor, Validator, OnDestroy {
+  formGroup: FormGroup;
+  isDisabled: boolean;
+
+  private subscriptions: Subscription[] = [];
+
+  constructor(private formBuilder: FormBuilder) {
+    this.formGroup = this.formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      phoneNumber: ['', Validators.required],
+      avatarUrl: ['', Validators.required],
+    });
+
+    this.subscriptions.push(
+      // Any time this form changes, indicate the change upstream and mark as touched
+      this.formGroup.valueChanges.subscribe((value: HeroFormValues): void => {
+        this.onChange(value);
+        this.onTouched();
+      }),
+    );
+  }
+
+  /**
+   * A callback method that performs clean-up, invoked immediately
+   * before Angular destroys the component
+   */
+  ngOnDestroy(): void {
+    for (const subscription of this.subscriptions) {
+      subscription.unsubscribe();
+    }
+  }
+
+  /**
+   * The registered callback function called when a change event occurs on the
+   * form control. The `registerOnChange` method performs the registration.
+   * Any implementation here gets overwritten upon registration.
+   */
+  onChange = (value: HeroFormValues): void => {};
+
+  /**
+   * The registered callback function called when a blur event occurs on the
+   * form control. The `registerOnTouched` method performs the registration.
+   * Any implementation here gets overwritten upon registration.
+   */
+  onTouched = (): void => {};
+
+  /**
+   * Registers a callback function the forms API calls on initialization to
+   * update the form model when values propagate from the view to the model
+   *
+   * @param fn Function to call when the control value changes
+   */
+  registerOnChange(fn: (value: HeroFormValues) => void): void {
+    this.onChange = fn;
+  }
+
+  /**
+   * Registers a callback function the forms API calls on initialization to
+   * update the form control on blur, also known as "touched"
+   *
+   * @param fn Function to call when touched
+   */
+  registerOnTouched(fn: () => void): void {
+    this.onTouched = fn;
+  }
+
+  /**
+   * Function the forms API calls when the control status changes to or from
+   * 'DISABLED'. Depending on the status, it enables or disables the appropriate
+   * DOM element
+   *
+   * @param isDisabled Whether to disable the DOM elements that constitute this
+   *   form control
+   */
+  setDisabledState(isDisabled: boolean): void {
+    if (isDisabled) {
+      this.formGroup.disable();
+    } else {
+      this.formGroup.enable();
+    }
+  }
+
+  /**
+   * Callback to determine the validity of the custom form control
+   *
+   * @returns Map of validation errors or null if the form control is valid
+   */
+  validate(): ValidationErrors | null {
+    if (this.formGroup.valid) {
+      return null;
+    }
+
+    return {
+      firstName: this.formGroup.get('firstName').errors,
+      lastName: this.formGroup.get('lastName').errors,
+      phoneNumber: this.formGroup.get('phoneNumber').errors,
+      avatarUrl: this.formGroup.get('avatarUrl').errors,
+    };
+  }
+
+  /**
+   * Writes a new value to the DOM elements that constitute this form control.
+   * The forms API calls this method to write to the view when programmatic
+   * changes from model to view occur.
+   */
+  writeValue(value: HeroFormValues): void {
+    if (value) {
+      this.formGroup.setValue(value, { emitEvent: false });
+    }
+  }
+}
